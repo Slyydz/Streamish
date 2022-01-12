02960ee69a319da38a71f924ee292f79cee5338f
Brady Williams <williams.cole29@yahoo.com>
2022-01-12 12:01:38 -0500

    Finished Chapter 6 lesson and first exercise

--- Streamish/Repositories/VideoRepository.cs
+++ Streamish/Repositories/VideoRepository.cs
@@ -359,7 +359,14 @@ namespace Streamish.Repositories
                 Id = DbUtils.GetInt(reader, "CommentId"),
                 Message = DbUtils.GetString(reader, "Message"),
                 VideoId = videoId,
-                UserProfileId = DbUtils.GetInt(reader, "CommentUserProfileId")
+                UserProfileId = DbUtils.GetInt(reader, "CommentUserProfileId"),
+                UserProfile = new UserProfile(){
+                    Id = DbUtils.GetInt(reader, "CommentUserProfileId"),
+                    Name = DbUtils.GetString(reader, "Name"),
+                    Email = DbUtils.GetString(reader, "Email"),
+                    DateCreated = DbUtils.GetDateTime(reader, "UserProfileDateCreated"),
+                    ImageUrl = DbUtils.GetString(reader, "UserProfileImageUrl"),
+                }
             };
 
             return comment;
---
+++ Streamish/client/src/components/Video.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card, CardBody } from "reactstrap";
+
+const Video = ({ video }) => {
+  return (
+    <Card >
+      <p className="text-left px-2">Posted by: {video.userProfile.name}</p>
+      <CardBody>
+        <iframe className="video"
+          src={video.url}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen />
+
+        <p>
+          <strong>{video.title}</strong>
+        </p>
+        <p>{video.description}</p>
+        <br>
+        </br>
+        <h4>Comments</h4>
+        <ul>
+        {video.comments.map(c => <li>{c.message} - Posted By: {c.userProfile.name}</li>)}
+        </ul>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default Video;
