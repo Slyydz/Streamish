94214fc51e0169e218a7ef8b3abb470e88dda662
Brady Williams <williams.cole29@yahoo.com>
2022-01-13 11:31:39 -0600

    Finished Routing section

--- Streamish/client/src/modules/VideoManager.js
+++ Streamish/client/src/modules/VideoManager.js
@@ -16,19 +16,28 @@ export const addVideo = (video) => {
 };
 
 export const getWithComments = () => {
-    return fetch(baseUrl + "/getwithcomments", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then((res) => res.json())
+  return fetch(baseUrl + "/getwithcomments", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then((res) => res.json())
 };
 
 export const getBySearch = (q, isSort) => {
   return fetch(baseUrl + `/search?q=${q}&sortDesc=${isSort}`, {
-      method: "GET",
-      headers: {
-          "Content-Type": "application/json"
-      }
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
   }).then((res) => res.json())
+}
+
+export const getVideoById = (videoId) => {
+  return fetch(baseUrl + `/GetVideoByIdWithComments/${videoId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(resp => resp.json())
 }
