e47d4656ed98a630df1a01db7ea33adfb52075bb
Brady Williams <williams.cole29@yahoo.com>
2022-01-14 13:10:58 -0600

    Finished Testing and Exercise 1

--- Streamish/Repositories/IUserProfileRepository.cs
+++ Streamish/Repositories/IUserProfileRepository.cs
@@ -1,10 +1,13 @@
 ﻿using Streamish.Models;
+using System.Collections.Generic;
 
 namespace Streamish.Repositories
 {
     public interface IUserProfileRepository
     {
         void Add(UserProfile userProfile);
+        void DeleteUser(int id);
+        List<UserProfile> Get();
         UserProfile GetByFirebaseIdWithVideo(string fireId);
         UserProfile GetByIdWithVideo(int id);
     }
