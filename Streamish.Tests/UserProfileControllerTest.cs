eaf14e45576591b7db4b1d764af2a0a776c2ac09
Brady Williams <williams.cole29@yahoo.com>
2022-01-14 14:11:09 -0600

    Finished First Exercise problem

--- Streamish.Tests/UserProfileControllerTest.cs
+++ Streamish.Tests/UserProfileControllerTest.cs
@@ -31,6 +31,72 @@ namespace Streamish.Tests
             Assert.Equal(testId, actualUser.Id);
         }
 
+        [Fact]
+        public void Get_All()
+        {
+            
+            var testData = CreateTestProfiles(20);
+
+            var repo = new InMemoryUserRepository(testData);
+            var controller = new UserProfileController(repo);
+
+            var result = controller.Get();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var actualUser = Assert.IsType<List<UserProfile>>(okResult.Value);
+
+            Assert.Equal(actualUser, testData);
+        }
+
+        [Fact]
+        public void Test_Delete()
+        {
+            var testValue = 99;
+            var testData = CreateTestProfiles(20);
+            testData[0].Id = testValue;
+
+            var repo = new InMemoryUserRepository(testData);
+            var controller = new UserProfileController(repo);
+
+            controller.Delete(testValue);
+
+            repo.InternalData.FirstOrDefault(p => p.Id == testValue);
+        }
+
+        [Fact]
+        public void Test_Register()
+        {
+
+            var testData = CreateTestProfiles(20);
+            
+            var repo = new InMemoryUserRepository(testData);
+            var controller = new UserProfileController(repo);
+
+            var newUser = CreateTestUserProfile(99);
+
+            controller.Register(newUser);
+            
+            Assert.Equal(testData.Last(), newUser);
+        }
+
+        [Fact]
+        public void Test_Get_Firebase()
+        {
+            var newFireId = "yup";
+            var testData = CreateTestProfiles(20);
+            testData[5].FirebaseUserId = newFireId;
+
+            var repo = new InMemoryUserRepository(testData);
+            var controller = new UserProfileController(repo);
+
+            var result = controller.GetByFireBaseUserId(newFireId);
+
+            var OkResult = Assert.IsType<OkObjectResult>(result);
+            var actualProfile = Assert.IsType<UserProfile>(OkResult.Value);
+
+            Assert.Equal(actualProfile, testData[5]);
+        }
+
         private List<UserProfile> CreateTestProfiles(int count)
         {
             var users = new List<UserProfile>();
